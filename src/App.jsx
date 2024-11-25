b090bd462c89df4c3d644aaa1710466d7a05b693
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-11-25 08:01:57 +0600

    Installed tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <div className="flex flex-col md:flex-row">
+        {/* Dashboard sidebar */}
+        <div className="md:w-64 md:min-h-screen bg-green-50">
+          <ul className="menu p-12">
+            <li>Home</li>
+          </ul>
+        </div>
+        <div className="md:p-8 p-2 flex-1">
+          {/* Here will be the contents */}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
