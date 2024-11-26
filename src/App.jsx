29df7941a811799ab60786ef5649072983b1fcd0
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-11-26 16:43:39 +0600

    Adding sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ function App() {
     <>
       <div className="flex flex-col md:flex-row">
         {/* Dashboard sidebar */}
-        <div className="md:w-64 md:min-h-screen bg-green-50">
-          <ul className="menu p-12">
-            <li>Home</li>
+        <div className="md:w-64 md:min-h-screen shadow-xl">
+          <ul className="menu py-12 px-4">
+            <li className="font-bold text-5xl flex text-[#333333]">
+              Sedap <span className="text-[#00B074]">.</span>
+            </li>
+            <li className="mt-4 text-[18px] text-[#B9BBBD] font-medium">
+              Modern Admin Dashboard
+            </li>
+            <li className="mt-20">Dashboard</li>
           </ul>
         </div>
         <div className="md:p-8 p-2 flex-1">
